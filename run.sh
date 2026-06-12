8be73a1b7d7844d540343f39267a3c561d3f15e4
Polina Smirnova <podmsmirnova@edu.hse.ru>
2026-06-12 15:36:46 +0000

    добавлены команды в run.sh по заданию

---
+++ run.sh
@@ -0,0 +1,7 @@
+if [ "$1" == "build_generator" ; then
+docker build -t generat ./generator
+elif [ "$1" == "run_generator" ]; then
+docker run --rm -v "$(pwd)/data:/app/data" generat
+elif [ "$1" == "create_local_data" ]; then
+python3 generator/generate.py local_data
+fi
