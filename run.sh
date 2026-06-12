20239e13a3cd237d2bbd4fe485590ce88a5f0409
Polina Smirnova <podmsmirnova@edu.hse.ru>
2026-06-12 18:38:41 +0000

    дописан файл run.sh, добавлены 2 необходимые команды для reporter

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ elif [ "$1" == "run_generator" ]; then
 docker run --rm -v "$(pwd)/data:/app/data" generat
 elif [ "$1" == "create_local_data" ]; then
 python3 generator/generate.py local_data
+elif [ "$1" == "build_reporter" ]; then
+docker build -t reporter ./reporter
+elif [ "$1" == "run_reporter" ]; then
+docker run --rm -v "$(pwd)/data:/data" reporter
 fi
